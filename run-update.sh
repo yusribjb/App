10a5811e84e2ed4cd6fc202935b1e403ba4f2a64
yusribjb <yusribjb@gmail.com>
2020-04-24 16:33:16 +0800

    Delete run-update.sh

--- run-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#HAPUS UPDATE
-rm -rf /root/update.sh
-#UPDATE UBUNTU,VIRTUAL HOST, CRONTAB, FIREFOX DAN PROFILE, SCRIPT PHP, HOST DAN PROXY
-wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/update.sh -O /root/update.sh
-chmod +x /root/update.sh
-/root/update.sh
